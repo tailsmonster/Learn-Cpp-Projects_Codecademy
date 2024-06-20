29096a7b0c6488520251a360462f782989f40de6
nico <lotailsmonsieur62c900@gmail.com>
2024-06-20 12:05:30 -0400

    added project 1

---
+++ 1-Initials/initials.cpp
@@ -0,0 +1,11 @@
+//Fun Fact: I got my 4 wisdom teeth removed on the same day!
+
+#include <iostream>
+int main(){
+  std::cout << "N   N    A  \n";
+  std::cout << "NN  N   A A \n";
+  std::cout << "N N N  AAAAA\n";
+  std::cout << "N  NN  A   A\n";
+  std::cout << "N   N  A   A\n";
+  std::cout << "N   N  A   A\n";
+}
