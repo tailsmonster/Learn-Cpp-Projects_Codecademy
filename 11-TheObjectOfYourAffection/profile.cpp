23355d6a1b03d8eaf7695d98b1d7b4e23ded6bed
tailsmonster12 <aroca.twelve@proton.me>
2024-08-01 09:15:49 -0400

    added project 11

---
+++ 11-TheObjectOfYourAffection/profile.cpp
@@ -0,0 +1,26 @@
+#include "profile.hpp"
+#include <string>
+#include <vector>
+
+Profile::Profile(std::string new_name, int new_age, std::string new_city, 
+                 std::string new_country, std::string new_pronouns, 
+                 std::vector<std::string> new_hobbies)
+    : name(new_name), age(new_age), city(new_city), country(new_country), 
+      pronouns(new_pronouns), hobbies(new_hobbies) {}
+
+std::string Profile::view_profile() const {
+    std::string bio = "Name: " + name + "\n";
+    bio += "Age: " + std::to_string(age) + "\n";
+    bio += "City: " + city + "\n";
+    bio += "Country: " + country + "\n";
+    bio += "Pronouns: " + pronouns + "\n";
+    bio += "Hobbies:\n";
+    for (const auto& hobby : hobbies) {
+        bio += "- " + hobby + "\n";
+    }
+    return bio;
+}
+
+void Profile::add_hobby(std::string new_hobby) {
+    hobbies.push_back(new_hobby);
+}
